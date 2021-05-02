02415590603477029e207d58fcf4b6f336860eac
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-02 15:53:32 -0500

    Navbar styled and routed.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,24 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
+import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div>
-    <Header />
-      <Router>
-      
-          <Route exact path={["/", "/About"]} component={About} />
-          <Route exact path='/portfolio' component={Portfolio} />
-          <Route exact path='/contact' component={Contact} />
-   
-      </Router>
+    <Router>
+      <Header />
+      <Route exact path={["/", "/Home"]} component={Home} />
+      <Route exact path='/portfolio' component={Portfolio} />
+      <Route exact path='/contact' component={Contact} />
       <Footer />
-      </div>
+    </Router>
   );
 }
 
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import './style.css';
 
 function Header() {
-    return <h1>This is a Header</h1>
+    const location = useLocation();
+
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+            <a className="navbar-brand">
+                <Link to="/home" className={location.pathname === "/" ? "nav-link active" : "nav-link"} id="navbar-brand">
+                Caitlin Collins
+                </Link>
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav navigation">
+                    <li className="nav-item">
+                        <Link to="/home" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                        Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                        Portfolio
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                        Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+ )
 }
 
 
