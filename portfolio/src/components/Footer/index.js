f0c948184eea88d188508486c9611ed53dca8f3e
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-02 15:27:07 -0500

    Footer complete

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,14 +1,37 @@
 import React from "react";
+import './style.css';
 
 function Footer() {
-    return <h1>This is a Footer</h1>
+    return (
+    <div className="row">
+        <div className="col-sm-12">
+            <footer>
+                © Copyright 2021 Caitlin Collins
+                <a
+                    href="https://www.facebook.com/caitlincollinsgraber"
+                    target="_blank"
+                    className="fa fa-facebook icon"
+                ></a>
+                <a
+                    href="https://www.instagram.com/caitlincollinsactor/"
+                    target="_blank"
+                    className="fa fa-instagram icon"
+                ></a>
+                <a
+                    href="https://www.linkedin.com/in/caitlin-collins-actorprogrammer/"
+                    target="_blank"
+                    className="fa fa-linkedin icon"
+                ></a>
+                <a
+                    href="https://github.com/CaitlinCollins"
+                    target="_blank"
+                    className="fa fa-github-square icon"
+                ></a>
+            </footer>
+        </div>
+    </div>
+    )
 }
-
-
-
-
-
-
 
 
 export default Footer;
