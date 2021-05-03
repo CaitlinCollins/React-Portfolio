50ca7a138917c35e96b6ecc9e52519528de51722
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-03 12:45:36 -0500

    Contact page rendering

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,7 +1,57 @@
 import React from "react";
+import "./style.css";
 
 function Contact() {
-    return <h1>This is a Contact</h1>
+    return (
+    <div className="row">
+        <div className="col-sm-6">
+            <article className="cont">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1>Contact</h1>
+                        <hr/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <form>
+                            <div className="form-group">
+                            <label for="exampleFormControlInput1">Name</label><input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name"></input>
+                            </div>
+                            <div className="form-group">
+                            <label for="exampleFormControlInput1">Email Address</label><input type="email" class="form-control" id="exampleInputEmail" placeholder="email@email.com"></input>
+                            </div>
+                            <div className="form-group">
+                            <label for="exampleFormControlTextarea1">Message</label><textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder="Your Message Goes Here!"></textarea>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-2">
+                                <button class="btn btn-secondary contactbtn" type="submit">Submit</button>
+                                </div>
+                                <div className="col-sm-10"> 
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div className="col-sm-4">
+            <aside>
+                <h3>Email: </h3>
+                <a href="mailto:caitlin.aileen86@gmail.com" target="_blank" class="contactP">caitlin.aileen86@gmail.com</a>
+                <h3>LinkedIn: </h3> 
+                <a href="https://www.linkedin.com/in/caitlin-collins-stpaul/" target="_blank" class="contactP">www.linkedin.com/in/caitlin-collins-stpaul</a>
+                <h3>GitHub: </h3>
+                <a href="https://github.com/CaitlinCollins" target="_blank" class="contactP">github.com/CaitlinCollins</a>
+                <h3>Phone: </h3>
+                <p class="contactP">612.916.5099</p>
+            </aside>
+        </div>
+        <div className="col-sm-2">
+        </div>
+    </div>
+)
 }
 
 
--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,7 +1,75 @@
 import React from "react";
+import './style.css';
 
-function About() {
-    return <h1>This is About Me.</h1>
+function Home() {
+    return (
+      <div className="row">
+        <div className="col-sm-10">
+            <article className="about card">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1>About Me<span><a
+                            href="public/assets/Caitlin_Collins_Full_Stack_Resume.pdf"
+                            download="Cait_Collins_Resume"
+                            target="_blank"
+                            className="btn btn-outline download"
+                            >Download Resume!</a></span>
+                        </h1>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <figure>
+                            <img className="img-fluid bioimg" src="assets/Caitlin.JPG" alt="Photo of Caitlin"/>
+                            <figcaption>Coming soon to a computer near you!</figcaption>
+                        </figure>
+                    </div>
+                    <div className="col-md-6">
+                        <p className="firstP">
+                            I'm a Full-Stack Computer Programmer specializing in creative
+                            concepts and user-friendly, responsive layouts.
+                            <br />
+                            I also work locally as an actor, model, writer, and film
+                            producer which keeps my head in the creative game.
+                            <br />
+                            Programming allows me to flex my problem-solving muscles while
+                            honing in on my creative instincts.
+                        </p>
+                    <div className="languages">Languages:
+                        <ul>
+                            <li className="item">HTML</li>
+                            <li className="item">CSS</li>
+                            <li className="item">JavaScript</li>
+                        </ul>
+                    </div>
+                    <div className="tools">Tools:
+                        <ul>
+                            <li className="item">JQuery</li>
+                            <li className="item">Node.js</li>
+                            <li className="item">Handlebars.js</li>
+                            <li className="item">MySql</li>
+                            <li className="item">Seqelize</li>
+                        </ul>
+                    </div>
+                    <div className="tools2">
+                        <ul>
+                            <li className="item">Express</li>
+                            <li className="item">APIs</li>
+                            <li className="item">Bootstrap</li>
+                            <li className="item">GitHub</li>
+                            <li className="item">Heroku</li>
+                        </ul>
+                    </div>
+                    <p className="tag">
+                    Coming soon to a computer near you!
+                    </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>       
+)
 }
 
 
@@ -11,4 +79,4 @@ function About() {
 
 
 
-export default About;
+export default Home;
