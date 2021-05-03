66eede700e1ab3e166e74df9d062a424000cffd3
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-03 13:52:24 -0500

    Added a project.json file

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { Container, Row, Col } from "../Grid";
 
 function Portfolio() {
-   return <h1>This is a Portfolio</h1>
+   return (
+         <Container>
+            <Row>
+               <Col size="xs-12 sm-12">
+                  <article className="port">
+                     <Row>
+                        <Col size="xs-12 sm-12">
+                           <h1>Portfolio</h1>
+                           <hr />
+                        </Col>
+                     </Row>
+                     <figure>
+                        <Col size="xs-12 sm-12">
+                           <div className="card text-center">
+                           <img
+											class="card-img-top img-fluid portimg"
+											src="assets/crate.png"
+											alt="Reel 2 Real"/>
+										<div class="card-body">
+											<h5 class="card-title">Reel 2 Real</h5>
+											<p class="card-text">
+												An online record database that allows the user to sign up or login for complete access. There is a search feature that allows the user to search by artist, which renders the albums listed under that artist. The user can then get more information about the album of their choice or add it to their crate to be saved for later. 
+											</p>
+											<a
+												href="https://reel2real.herokuapp.com/"
+												target="_blank"
+												class="btn deploy"
+												>Deploy Here</a>
+											<a
+												href="https://github.com/CaitlinCollins/reel2real"
+												target="_blank"
+												class="btn deploy"
+												>GitHub Repo</a>
+                                 </div>
+                           </div>
+                        </Col>
+                     </figure>
+                  </article>
+               </Col>
+            </Row>
+         </Container>
+      )
 }
 
 
