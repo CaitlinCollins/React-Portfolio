aef13b80067435fc29ecd0f11637a3499e2bd6d8
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-05 20:25:03 -0500

    Updated with HashRouter

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -9,13 +10,13 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Header />
       <Route exact path={["/", "/Home"]} component={Home} />
       <Route exact path='/portfolio' component={Portfolio} />
       <Route exact path='/contact' component={Contact} />
       <Footer />
-    </Router>
+    </HashRouter>
   );
 }
 
